8dc413db336f0f783cb80613772794c8c3126648
goguide <>
2019-06-09 20:09:06 +0300

    Tweak for gh-pages

--- i18n-gen.js
+++ i18n-gen.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-let source_dir = './i18n/';
-let bundle = './i18n.css';
-let manifest = './i18n.json';
+let source_dir = './src/i18n/';
+let bundle = './docs/i18n.css';
+let manifest = './docs/i18n.json';
 let available_languages = {};
 
 fs.unlink(bundle, ()=>{});
--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import copy from 'rollup-plugin-copy'
 export default {
     input: 'src/main.js',
     output: {
-        file: './bundle.js',
+        file: 'docs/bundle.js',
         format: 'iife'
     },
     plugins: [
         copy({
             targets: ['src/index.html'],
-            outputFolder: './'
+            outputFolder: 'docs'
         })
     ]
 }
